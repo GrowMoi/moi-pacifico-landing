8704f2be54e9dba8820eaf1b31a8554b2d8d866d
Darwin Gualotuña <daosgc@gmail.com>
2020-01-23 15:14:54 -0500

    Add terms and conditions page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TermsConditionsComponent } from './terms-conditions/terms-conditions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'termsAndConditions', component: TermsConditionsComponent, pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
